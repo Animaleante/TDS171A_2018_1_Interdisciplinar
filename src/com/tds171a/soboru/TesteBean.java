36969b7a48fa74f11b5f83583b2e3ae7ecf252f2
Diogo S. Moraes <srto.dih@gmail.com>
2018-05-06 14:39:41 -0300

    Mudando pra Dynamic Web Project

---
+++ src/com/tds171a/soboru/TesteBean.java
@@ -0,0 +1,14 @@
+package com.tds171a.soboru;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named("testBean")
+@RequestScoped
+public class TesteBean {
+
+	public TesteBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
